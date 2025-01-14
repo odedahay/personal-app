c066d027aa5fbbc0ebc2de5451a7fffb962acac7
odedahay <odedahay@yahoo.com>
2025-01-13 19:54:56 -0700

    re-eployment Configuration

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,32 +1,37 @@
-// "use client";
 import { Metadata } from 'next';
 import { getBlogBySlug, getBlogsSlug, getBlogBySlugWithMarkdown } from '@/lib/blogs';
 import { Blog } from '@/interfaces/Blog';
 import { BlogHeader } from '@/components/blogs';
 import { PageLayout } from '@/components/layouts';
 
-// type Props = {params: { slug: string }; };
-type Params = Promise<{ slug: string }>
+type Props = { params: { slug: string } };
 
-export const metadata: Metadata = {
-  title: 'Blog'
+// Pre-generate all static paths
+export const generateStaticParams = async () => {
+  const slugs: string[] = await getBlogsSlug();
+  return slugs.map((slug) => ({ slug }));
 };
 
-const BlogDetail = async (props: { params: Params }) => {
-  const { slug } = await props.params;
-  const blog: Blog | null = await getBlogBySlug(slug);
-  const blogHTML = blog? await getBlogBySlugWithMarkdown(slug): null;
+// Generate dynamic metadata
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const blog = await getBlogBySlug(params.slug);
+  return {
+    title: blog ? blog.title : "Blog's page not found",
+  };
+};
 
-  if (blog) {
-    metadata.title = blog.title;
-  } else {
+const BlogDetail = async ({ params }: Props) => {
+  const { slug } = await params;
+  const blog: Blog | null = await getBlogBySlug(slug);
+  const blogHTML = blog ? await getBlogBySlugWithMarkdown(slug) : null;
+
+  if (!blog) {
     return (
       <PageLayout>
         <div className="text-center mt-10 text-xl">Blog's page not found.</div>
       </PageLayout>
     );
   }
-
 
   return (
     <PageLayout>
