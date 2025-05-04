308e96135265b93ea42e5fd36b8f9b2f7c3394d7
odedahay <odedahay@yahoo.com>
2025-05-04 05:03:20 -0600

    Add new blog post on HTML Email Design & Development, including detailed content, images, and search index updates. Enhance blog detail page with a 'Back to Blog List' button.

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -46,6 +46,18 @@ const BlogDetail = async (props: { params: Props }) => {
           {/* Render blog content as HTML */}
           <div dangerouslySetInnerHTML={{ __html: blogHTML?.content || '' }} />
         </article>
+        <div className="flex justify-center mt-8">
+  <a
+    href="/blogs"
+    className="inline-block px-4 py-2 text-lg bg-pink-400 text-white rounded-lg font-medium hover:bg-gray-200 hover:text-black transition"    
+  >
+    <div className='flex items-center'>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg> 
+    Back to Blog List
+    </div>
+    
+  </a>
+</div>
       </div>
     </PageLayout>
   );
