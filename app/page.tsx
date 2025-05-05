dd3833d24bb69a18f7adb124cbd874932da0a2b2
odedahay <odedahay@yahoo.com>
2025-05-05 17:05:00 -0600

    Update Home page to display 8 blogs instead of 4, and remove the limited portfolios section. Add a new link to explore all design blog articles for improved navigation.

--- app/page.tsx
+++ app/page.tsx
@@ -20,14 +20,14 @@ const Home: NextPage = async () => {
   saveSearchData(content);
 
   // Limit to 4 blogs
-  const limitedBlogs = blogs.slice(0, 4);
+  const limitedBlogs = blogs.slice(0, 8);
 
   // Filter portfolios from blogs based on category "Portfolio"
   const portfolioBlogs = blogs.filter(blog => blog.category === "Portfolio");
 
   // Limit to 4 portfolios
   // const limitedPortfolios = portfolios.slice(0, 4);
-  const limitedPortfolios = portfolioBlogs.slice(0, 4);
+  //const limitedPortfolios = portfolioBlogs.slice(0, 4);
 
   return (
     <BaseLayout>
@@ -41,18 +41,24 @@ const Home: NextPage = async () => {
         <BlogList blogs={limitedBlogs}/>
       </div>
       <div className="flex flex-row justify-between items-center">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        {/* <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Portfolios
           <Link href="/blogs/category/Portfolio" className='text-sm ml-1 text-pink-500'>
               (See All)
           </Link>
-        </h2>
+        </h2> */}
+         
+         <Link href="/blogs" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-pink-400  hover:bg-pink-600 md:px-10 
+          md:text-lg" target="_blank">
+              Explore All Design Blog Articles
+          </Link>
+
         <Link href="https://odedahay.github.io/assets/rodelio_dahay_portfolio.pdf" target="_blank" className='text-pink-500 rounded hover:text-white px-3 py-[5px] hover:bg-pink-700 text-sm font-bold '>
               View My Old Portfolio
           </Link>
       </div>
       {/* <PortfolioList portfolios={limitedPortfolios} /> */}
-      <BlogList blogs={limitedPortfolios} />
+      {/* <BlogList blogs={limitedPortfolios} /> */}
     </BaseLayout>
   )
 }
